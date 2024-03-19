4f18e42020f7059247724f97549e3d63c7d3c099
Sai Rohit <sairohit11.s@gmail.com>
2024-03-20 00:30:32 +0530

    title addded

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,14 @@ import NavBar from "@/modules/nav-bar";
 import { globalStyles } from "@/styles/global-styles";
 import { Global } from "@emotion/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Sai Rohit</title>
+      </Head>
       <Global styles={globalStyles} />
       <Header />
       <NavBar />
