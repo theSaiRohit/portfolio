60ab5307e8256500099532e32788a8e0e3c5f505
Sai Rohit <sairohit11.s@gmail.com>
2024-03-20 00:21:22 +0530

    properly redirected

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
-import Logo from "@/components/logo";
 import HomePageModule from "@/modules/home";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/under-development");
+    return;
+  });
   return <HomePageModule />;
 }
--- pages/under-development.tsx
+++ pages/under-development.tsx
@@ -1,11 +1,5 @@
 import UnderDev from "@/modules/under-dev";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 export default function UnderDevelopment() {
-  const router = useRouter();
-  useEffect(() => {
-    router.push("/under-development");
-  });
   return <UnderDev />;
 }
