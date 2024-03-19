72b37c5e3d46648da26336b2a81400d3c3e7dd98
Sai Rohit <sairohit11.s@gmail.com>
2024-03-20 00:15:04 +0530

    under dev

--- pages/under-development.tsx
+++ pages/under-development.tsx
@@ -1,5 +1,11 @@
 import UnderDev from "@/modules/under-dev";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function UnderDevelopment() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/under-development");
+  });
   return <UnderDev />;
 }
