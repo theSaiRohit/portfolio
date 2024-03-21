51908b7a125940ac67573379fa3a88589a323462
Sai Rohit <sairohit11.s@gmail.com>
2024-03-21 22:23:16 +0530

    under dev redirect 2.0

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Home() {
-  // const router = useRouter();
-  // useEffect(() => {
-  //   router.push("/under-development");
-  //   return;
-  // });
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/under-development");
+    return;
+  });
   return <HomePageModule />;
 }
