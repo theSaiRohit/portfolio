52fca90fe85a7abcc88dae115342f0c25e4da1a0
Sai Rohit <sairohit11.s@gmail.com>
2024-03-31 17:03:21 +0530

    build error fixed

--- modules/under-dev/index.tsx
+++ modules/under-dev/index.tsx
@@ -1,6 +1,5 @@
 import HomeLandingBg from "@/modules/home/home-landing-bg";
 import { h1SpanCss, h1WrapperCss, h2Css, mainCss } from "@/modules/home/home-landing-bg/home-main/styles";
-import { blankElemCss } from "@/modules/home/styles";
 import { underDevH2, underDevMainText } from "@/modules/under-dev/content";
 
 export default function UnderDev() {
@@ -18,7 +17,6 @@ export default function UnderDev() {
         <h2 css={h2Css}>{underDevH2}</h2>
       </main>
       <HomeLandingBg />
-      <div css={blankElemCss}></div>
     </>
   );
 }
