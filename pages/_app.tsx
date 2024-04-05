3ea9cc7ae8f831404b831389b3688e070d6af535
Sai Rohit <sairohit11.s@gmail.com>
2024-04-05 18:53:14 +0530

    fixed resize bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,20 +5,8 @@ import { globalStyles } from "@/styles/global-styles";
 import { Global } from "@emotion/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  useEffect(() => {
-    const { addEventListener, removeEventListener, scrollTo } = window;
-    const appResizeHandler = () => {
-      scrollTo(0, 0);
-      // router.reload();
-    };
-    addEventListener("resize", appResizeHandler);
-    return () => removeEventListener("resize", appResizeHandler);
-  }, [router]);
   return (
     <>
       <Head>
