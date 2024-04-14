2fd3a4147caa2f9ee47ea7f28a2ec0f7d6078326
Sai Rohit <sairohit11.s@gmail.com>
2024-04-14 13:41:25 +0530

    inspire text changed

--- modules/footer/footer-credits/content.ts
+++ modules/footer/footer-credits/content.ts
@@ -1,3 +1,3 @@
 export const inspirationPortfolioLink = "https://yasingenc.net/";
-export const inspoText = "Design inspired from";
+export const inspoText = "Design based on";
 export const inspoPortfolioName = "Yasin Genc";
