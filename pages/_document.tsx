9a2ccc70210ae52e796f32bb44c5f5b1d92518d9
Sai Rohit <sairohit11.s@gmail.com>
2024-11-08 12:34:13 +0530

    add script to app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          strategy="beforeInteractive"
+          src="https://cdn-stag.optimeleon.com/por-8dwep/def-8dwep/main.js"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
