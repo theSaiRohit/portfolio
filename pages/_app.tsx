62ac66fa1be7ff542288919c903b43f39dcb34e9
Sai Rohit S <sairohit11.s@gmail.com>
2025-07-31 13:19:52 +0530

    add optimeleon scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,14 +11,36 @@ import Script from "next/script";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src="https://workers.sairohit11-s.workers.dev"
-      />
       <GlobalContextProvider>
         <Head>
           <title>Sai Rohit</title>
         </Head>
+        {/* Load Optimeleon script dynamically */}
+        <Script
+          id="optimeleon-loader"
+          strategy="afterInteractive"
+          src="https://cdn-stag.optimeleon.com/ser-r31sv/sai-r31t5/v1.main.js"
+          async
+        />
+        <Script
+          id="optimeleon-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,n,o,s,a,c){
+                e.optimeleon||(s=e.optimeleon=function(){
+                  s.callMethod?s.callMethod.apply(s,arguments):s.queue.push(arguments)
+                },
+                s.push=s,s.queue=[],
+                (a=t.createElement(n)).async=!0,
+                a.src="https://cdn-stag.optimeleon.com/ser-r31sv/sai-r31t5/v1.main.js",
+                (c=t.getElementsByTagName(n)[0]).parentNode.insertBefore(a,c))
+              }(window,document,"script");
+              optimeleon("init",true,true);
+            `,
+          }}
+        />
+
         <Global styles={globalStyles} />
         <Header />
         <NavBar />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,38 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <Script
-          strategy="beforeInteractive"
-          src="https://cdn-stag.optimeleon.com/mer-gryuk/sai-gryup/main.js"
-        />
-        <Script
-          id="abhide"
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-            var timeout = 3000;
-            !function(h,i,d,e){var t,n=h.createElement("style");n.id=e,n.innerHTML="body{opacity:0}",h.head.appendChild(n),t=d,i.rmfk=function(){var t=h.getElementById(e);t&&t.parentNode.removeChild(t)},setTimeout(i.rmfk,t)}(document,window,timeout,"abhide");
-          `
+              !(function(h, i, e) {
+                var t = 2000;
+                var n = h.createElement("style");
+                n.id = e;
+                n.innerHTML = "body{opacity:0}";
+                h.head.appendChild(n);
+                i.rmfk = function () {
+                  var t = h.getElementById(e);
+                  t && t.parentNode.removeChild(t);
+                };
+                setTimeout(i.rmfk, t);
+              })(document, window, "optimeleon-overlay");
+            `,
           }}
-        /> */}
+        />
+
+        {/* Set OptiCookieConsent function */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.setOptiCookieConsent = (consent) => {
+                localStorage.setItem("opti_consent", consent);
+              };
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
