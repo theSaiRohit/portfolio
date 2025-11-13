4fdca754b37e439a44d63b244682458eb9b0a8fb
Sai Rohit S <sairohit11.s@gmail.com>
2025-11-13 17:11:55 +0530

    implement Optimeleon script loading and consent handling in App component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,47 @@ export default function App({ Component, pageProps }: AppProps) {
         <Head>
           <title>Sai Rohit</title>
         </Head>
+                <Script id="set-opticookie-fn" strategy="afterInteractive">
+          {`
+            window.setOptiCookieConsent = function(consent) {
+              localStorage.setItem("opti_consent", consent);
+            };
+          `}
+        </Script>
+
+        {/* Load Optimeleon main script */}
+        <Script
+          id="optimeleon-main"
+          src="https://cdn-stag.optimeleon.com/sai-x7jcx/sai-x7jcx/v1.main.js"
+          async
+          strategy="afterInteractive"
+        />
+
+        {/* Initialize Optimeleon */}
+        <Script id="optimeleon-init" strategy="afterInteractive">
+          {`
+            (function(e,t,n,o,c,a,s){
+              if(!e.optimeleon){
+                c = e.optimeleon = function(){
+                  c.callMethod ? c.callMethod.apply(c, arguments) : c.queue.push(arguments);
+                };
+                c.push = c;
+                c.queue = [];
+                a = t.createElement(n);
+                a.async = true;
+                a.src = "https://cdn-stag.optimeleon.com/sai-x7jcx/sai-x7jcx/v1.main.js";
+                s = t.getElementsByTagName(n)[0];
+                s.parentNode.insertBefore(a, s);
+              }
+            })(window, document, "script");
+            optimeleon("init", true, true);
+          `}
+        </Script>
+
+        {/* Set consent immediately */}
+        <Script id="optimeleon-consent" strategy="afterInteractive">
+          {`window.setOptiCookieConsent("yes");`}
+        </Script>
         <Global styles={globalStyles} />
         <Header />
         <NavBar />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <Head />
       <body>
         <Main />
         <NextScript />
