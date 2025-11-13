b9f8a8fc0f2bf0a6803782b55f9055da3bbe52a3
Sai Rohit S <sairohit11.s@gmail.com>
2025-11-13 17:06:58 +0530

    refactor Optimeleon script loading and set cookie consent immediately

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,61 +7,62 @@ import { Global } from "@emotion/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Script from "next/script";
-import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  // @ts-ignore
-  useEffect(() => { window.CFQ = window.CFQ || []; window.CFQ.push({ emit: 'pageHydrated' }) }, [])
+
   return (
     <>
       <GlobalContextProvider>
         <Head>
           <title>Sai Rohit</title>
         </Head>
-        {/* Load Optimeleon script dynamically */}
+
+        {/* --- Load Optimeleon Scripts Properly --- */}
+
+        {/* Define helper function */}
+        <Script id="set-opticookie-fn" strategy="afterInteractive">
+          {`
+            window.setOptiCookieConsent = function(consent) {
+              localStorage.setItem("opti_consent", consent);
+            };
+          `}
+        </Script>
+
+        {/* Load Optimeleon main script */}
         <Script
-          id="optimeleon-loader"
-          strategy="afterInteractive"
-          src="https://webappdatastaging.blob.core.windows.net/sai-x7jcx/sai-x7jcx/debug/v1.main.js"
+          id="optimeleon-main"
+          src="https://cdn-stag.optimeleon.com/sai-x7jcx/sai-x7jcx/v1.main.js"
           async
-        />
-        <Script
-          id="optimeleon-init"
           strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(e,t,n,o,s,a,c){
-                e.optimeleon||(s=e.optimeleon=function(){
-                  s.callMethod?s.callMethod.apply(s,arguments):s.queue.push(arguments)
-                },
-                s.push=s,s.queue=[],
-                (a=t.createElement(n)).async=!0,
-                a.src="https://webappdatastaging.blob.core.windows.net/sai-x7jcx/sai-x7jcx/debug/v1.main.js",
-                (c=t.getElementsByTagName(n)[0]).parentNode.insertBefore(a,c))
-              }(window,document,"script");
-              optimeleon("init",true,true);
-            `,
-          }}
         />
-        <Script
-  id="set-opti-cookie-consent"
-  strategy="afterInteractive"
-  dangerouslySetInnerHTML={{
-    __html: `
-      window.setOptiCookieConsent = (consent) => {
-        localStorage.setItem("opti_consent", consent);
-      };
 
-      // Automatically set a default consent if none exists
-      if (!localStorage.getItem("opti_consent")) {
-        window.setOptiCookieConsent("true");
-      }
-    `,
-  }}
-/>
+        {/* Initialize Optimeleon */}
+        <Script id="optimeleon-init" strategy="afterInteractive">
+          {`
+            (function(e,t,n,o,c,a,s){
+              if(!e.optimeleon){
+                c = e.optimeleon = function(){
+                  c.callMethod ? c.callMethod.apply(c, arguments) : c.queue.push(arguments);
+                };
+                c.push = c;
+                c.queue = [];
+                a = t.createElement(n);
+                a.async = true;
+                a.src = "https://cdn-stag.optimeleon.com/sai-x7jcx/sai-x7jcx/v1.main.js";
+                s = t.getElementsByTagName(n)[0];
+                s.parentNode.insertBefore(a, s);
+              }
+            })(window, document, "script");
+            optimeleon("init", true, true);
+          `}
+        </Script>
 
+        {/* Set consent immediately */}
+        <Script id="optimeleon-consent" strategy="afterInteractive">
+          {`window.setOptiCookieConsent("yes");`}
+        </Script>
 
-
+        {/* --- Global styles & Layout --- */}
         <Global styles={globalStyles} />
         <Header />
         <NavBar />
