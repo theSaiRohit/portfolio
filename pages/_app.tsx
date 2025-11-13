9f0ffabddae9c2d26a05a413887487390fbe5650
Sai Rohit S <sairohit11.s@gmail.com>
2025-11-13 17:09:19 +0530

    remove Optimeleon script loading and consent handling from App component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,53 +16,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <Head>
           <title>Sai Rohit</title>
         </Head>
-
-        {/* --- Load Optimeleon Scripts Properly --- */}
-
-        {/* Define helper function */}
-        <Script id="set-opticookie-fn" strategy="afterInteractive">
-          {`
-            window.setOptiCookieConsent = function(consent) {
-              localStorage.setItem("opti_consent", consent);
-            };
-          `}
-        </Script>
-
-        {/* Load Optimeleon main script */}
-        <Script
-          id="optimeleon-main"
-          src="https://cdn-stag.optimeleon.com/sai-x7jcx/sai-x7jcx/v1.main.js"
-          async
-          strategy="afterInteractive"
-        />
-
-        {/* Initialize Optimeleon */}
-        <Script id="optimeleon-init" strategy="afterInteractive">
-          {`
-            (function(e,t,n,o,c,a,s){
-              if(!e.optimeleon){
-                c = e.optimeleon = function(){
-                  c.callMethod ? c.callMethod.apply(c, arguments) : c.queue.push(arguments);
-                };
-                c.push = c;
-                c.queue = [];
-                a = t.createElement(n);
-                a.async = true;
-                a.src = "https://cdn-stag.optimeleon.com/sai-x7jcx/sai-x7jcx/v1.main.js";
-                s = t.getElementsByTagName(n)[0];
-                s.parentNode.insertBefore(a, s);
-              }
-            })(window, document, "script");
-            optimeleon("init", true, true);
-          `}
-        </Script>
-
-        {/* Set consent immediately */}
-        <Script id="optimeleon-consent" strategy="afterInteractive">
-          {`window.setOptiCookieConsent("yes");`}
-        </Script>
-
-        {/* --- Global styles & Layout --- */}
         <Global styles={globalStyles} />
         <Header />
         <NavBar />
